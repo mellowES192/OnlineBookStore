ffafb837227a89306807ce7552d001e12b782893
Anton Nagornyi <antoha_sapela@mail.ru>
2022-03-15 17:13:55 +0300

    Add Authorization

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -2,15 +2,36 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Data;
 using BookStore.Areas.Identity.Data;
+using BookStore.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("BookStoreContextConnection");builder.Services.AddDbContext<BookStoreContext>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<BookStoreUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<BookStoreContext>();
+    options.UseSqlServer(connectionString));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDefaultIdentity<BookStoreUser>().AddRoles<IdentityRole>().
+    AddEntityFrameworkStores<BookStoreContext>();
+//builder.Services.AddScoped<BookStoreContext>();
+//builder.Services.AddScoped <UserManager<BookStoreUser>>();
 
 var app = builder.Build();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<BookStoreContext>();
+        var userManager = services.GetRequiredService<UserManager<BookStoreUser>>();
+        DbInitializer.InitializeAsync(context, services, userManager).Wait();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Error");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -30,5 +51,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
